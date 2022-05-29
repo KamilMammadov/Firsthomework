ed6e2969bdd8647791a15a946a4d87de9fb0624c
unknown <kamiltm@code.edu.az>
2022-05-29 01:46:57 -0700

    valyuta task added

---
+++ Valyuta/Valyuta Homework/Valyuta Homework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Valyuta_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int amount = 10000;
+
+            double usd = amount / 1.7;
+
+            double euro = amount / 1.8282;
+
+            double ruble = amount * 38.68;
+
+            Console.WriteLine($"Current Amount : {amount} AZN");
+
+            Console.WriteLine($"US Dollar : {usd} USD");
+
+            Console.WriteLine($"European Union : {euro} Euro");
+
+            Console.WriteLine($"Russian Ruble : {ruble} Ruble");
+            
+
+        }
+    }
+}

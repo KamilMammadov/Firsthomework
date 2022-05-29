02bb8e04f6d08f1fb6aca035d928e7fb975cc10b
unknown <kamiltm@code.edu.az>
2022-05-29 01:51:29 -0700

    Faiz task added

---
+++ faiz/PercentTask/PercentTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PercentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long budget = 20000000000;
+
+            double mudNaz = budget * 0.2;
+
+            double texNaz = budget * 0.1;
+
+            double currentbudget = budget - (mudNaz + texNaz);
+
+            Console.WriteLine("Budce : "+budget);
+
+            Console.WriteLine("Mudafie Nazirliyine : "+mudNaz);
+
+            Console.WriteLine("Yüksek Texnologiyalar Nazirliyine : "+texNaz);
+
+            Console.WriteLine("Yerde qalan budce : "+currentbudget);
+
+        }
+    }
+}
